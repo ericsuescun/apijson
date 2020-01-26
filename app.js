f5b79d203961efb35d786d67bd78e26c5544e1ca
Eric Suescun <edsuescun@gmail.com>
2020-01-25 20:10:28 -0500

    Se agrega el encabezado de json

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ mongoose.connection.on("error", function(e) { console.error(e); });
 
 app.get('/products', async (req, res) => {
 	const productList = await Product.find();
+	res.header("Content-Type", "application/json");
 	res.json(productList);
 })
 
